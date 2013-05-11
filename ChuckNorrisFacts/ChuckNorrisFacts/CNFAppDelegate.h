b87e88af14f1cf9da962b5add7bd8450fd609596
Raphael Fleischlin <git@raefftec.ch>
2013-05-11 13:31:05 +0200

    Added the project

---
+++ ChuckNorrisFacts/ChuckNorrisFacts/CNFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CNFAppDelegate.h
+//  ChuckNorrisFacts
+//
+//  Created by JoeFlyclapper on 5/11/13.
+//  Copyright (c) 2013 RaffiAndStefanGmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CNFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ChuckNorrisFacts/ChuckNorrisFacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChuckNorrisFacts
+//
+//  Created by JoeFlyclapper on 5/11/13.
+//  Copyright (c) 2013 RaffiAndStefanGmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CNFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CNFAppDelegate class]));
+    }
+}
