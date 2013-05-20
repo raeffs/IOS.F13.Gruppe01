d7f84cc991922710761f6cefcd88867a064f1fe9
Raphael Fleischlin <git@raefftec.ch>
2013-05-20 13:52:17 +0200

    unit test project

---
+++ ChuckNorrisFacts/ChuckNorrisFactsTests/ChuckNorrisFactsTests.h
@@ -0,0 +1,13 @@
+//
+//  ChuckNorrisFactsTests.h
+//  ChuckNorrisFactsTests
+//
+//  Created by Raphael Fleischlin on 20.05.13.
+//  Copyright (c) 2013 S. Bachmann & R. Fleischlin. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ChuckNorrisFactsTests : SenTestCase
+
+@end
---
+++ ChuckNorrisFacts/ChuckNorrisFactsTests/ChuckNorrisFactsTests.m
@@ -0,0 +1,32 @@
+//
+//  ChuckNorrisFactsTests.m
+//  ChuckNorrisFactsTests
+//
+//  Created by Raphael Fleischlin on 20.05.13.
+//  Copyright (c) 2013 S. Bachmann & R. Fleischlin. All rights reserved.
+//
+
+#import "ChuckNorrisFactsTests.h"
+
+@implementation ChuckNorrisFactsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ChuckNorrisFactsTests");
+}
+
+@end
